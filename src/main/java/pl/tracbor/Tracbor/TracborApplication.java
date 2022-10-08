b3fcb14914e9ad54b7095d0a2430cf85d6587eef
Hubert Gawczyński <hubert.gawczynski@gmail.com>
2022-10-08 21:13:05 +0200

    first setup

---
+++ src/main/java/pl/tracbor/Tracbor/TracborApplication.java
@@ -0,0 +1,13 @@
+package pl.tracbor.Tracbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracborApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/tracbor/Tracbor/TracborApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tracbor.Tracbor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
